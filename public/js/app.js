f8bd85330b3e03dc20fa22573688bf2194e4e88c
Hannah93 <Hannelore.hauquier@hotmail.com>
2015-11-24 15:42:29 +0100

    changed name

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,2 @@
 // public/js/app.js
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'LeerkrachtCtrl','LeerlingCtrl', 'SignupCtrl','LeerkrachtService']);
+angular.module('EenBordVolRechten', ['ngRoute', 'appRoutes', 'MainCtrl', 'LeerkrachtCtrl','LeerlingCtrl', 'SignupCtrl','LeerkrachtService']);
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,8 +1,4 @@
 // public/js/controllers/MainCtrl.js
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
-  $scope.changestate = function (path) {
-          $state.go(path);
-      };
-
 });

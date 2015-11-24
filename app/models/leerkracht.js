feedfa9b6a8e254021abeda05f91660709e7aecd
Hannah93 <Hannelore.hauquier@hotmail.com>
2015-11-24 16:02:33 +0100

    deleted username

--- app/models/leerkracht.js
+++ app/models/leerkracht.js
@@ -4,10 +4,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var leerkrachtSchema=new Schema({
-   username: { type: String, required: true, index: { unique: true } },
   firstname : {type : String, required: true,},
   lastname : {type : String, required: true},
-  email : {type : String, required: true},
+  email : {type : String, required: true, index: { unique: true }},
   password: {type: String,required: true}
 });
 

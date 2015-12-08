88e2688ce04e7fe39012edd35e4facd8dbe86645
Hannah93 <Hannelore.hauquier@hotmail.com>
2015-12-08 21:28:29 +0100

    get anwsers multi

--- public/js/controllers/LeerkrachtPresentatie/LeerkrachtPresentatieMultiCtrl.js
+++ public/js/controllers/LeerkrachtPresentatie/LeerkrachtPresentatieMultiCtrl.js
@@ -1,12 +1,35 @@
 angular.module('LeerkrachtPresentatieMultiCtrl', []).controller('LeerkrachtPresentatieMultiController', ['$http', '$scope', '$routeParams','$location', function($http, $scope, $routeParams,$location)  {
   document.body.style.background = "#CEF6F5 url"
 
+ $scope.oplossingen=$scope.vraag.oplossingen;
+$scope.results = [];
+var amount=0;
 
   $scope.getAnswers=function(){
     $http.get('/api/AntwoordData/' + $scope.lesID +'/' + $scope.vraag._id)
         .success(function(data) {
               $scope.antwoorden = data;
-            console.log($scope.antwoorden);
+
+            $scope.results = [];
+            angular.forEach($scope.oplossingen, function(oplossing, key) {
+                  angular.forEach($scope.antwoorden, function(value, key) {
+                    console.log(oplossing+":"+value.antwoord);
+                        if(oplossing==value.antwoord){
+                          console.log("+1");
+                           amount = amount+1;
+                          console.log(amount);
+                        }
+                    });
+
+                    $scope.results.push({
+                      opl: oplossing,
+                      aantal:amount
+                  });
+                  console.log($scope.results);
+          });
+
+
+
         });
       };
 

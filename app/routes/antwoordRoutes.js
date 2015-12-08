6b873496b78363f61377515818225ef8389c9657
Airien <witeengel@hotmail.com>
2015-12-08 12:25:36 +0100

    antwoord routes

---
+++ app/routes/antwoordRoutes.js
@@ -0,0 +1,45 @@
+var models = require('../models/leerkracht');
+    module.exports = function(app, passport) {
+
+      app.post('/addAntwoord', function(req, res, next) {
+        console.log('in antwoord');
+          if (!req.body.antwoord) {
+              return res.json({ error: 'Vul aub antwoord in' });
+          }
+          models.Leerkracht.findById(req.session.leerkrachtID,function(err,leerkracht)
+          {
+            if(!leerkracht.lessen.id(req.session.lesID).vragen.id(req.body.vraagID).actief)
+            {
+              return res.json({ error: 'vraag is niet actief' });
+          }
+        });
+
+
+          var newAntwoord=new models.Antwoord();
+          newAntwoord.antwoord=req.body.antwoord;
+          newAntwoord.voornaam=req.session.voornaam;
+          newAntwoord.achternaam=req.session.achternaam;
+
+          newAntwoord.save(function (err){
+              if (err) {console.log(err);
+                return res.json({ error: 'error saving new antwoord' });
+                   console.log('error saving new antwoord');
+                   console.log(err);
+               }
+               else {
+                     console.log('new vraag saved successfully');
+
+                     models.Leerkracht.findById(req.session.leerkrachtID, function(err, leerkracht){
+                        leerkracht.lessen.id(req.session.lesID).vragen.id(req.body.vraagID).antwoorden.push(newAntwoord);
+                         leerkracht.save(function (err){
+                             if (err) {
+                               return res.json({ error: 'error adding new answer to list' });
+                             console.log('error adding new answer to list');
+                             console.log(err);
+                             }
+                         });
+                     });
+                 };
+              });
+          });
+    };

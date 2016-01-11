de1a3113acacd267335ac88b684942326187165e
Hannah93 <Hannelore.hauquier@hotmail.com>
2016-01-11 10:57:50 +0100

    leerkrachtpresentatie cloud visible again

--- public/js/controllers/LeerkrachtPresentatie/LeerkrachtPresentatieCloudCtrl.js
+++ public/js/controllers/LeerkrachtPresentatie/LeerkrachtPresentatieCloudCtrl.js
@@ -10,6 +10,15 @@ angular.module('LeerkrachtPresentatieCloudCtrl', []).controller('LeerkrachtPrese
       $scope.vraagID=$scope.vraag._id;
       console.log($scope.vraagID);
 
+      var word_array = [
+         {text: "Lorem", weight: 15},
+         {text: "Ipsum", weight: 9, link: "http://jquery.com/"},
+         {text: "Dolor", weight: 6, html: {title: "I can haz any html attribute"}},
+         {text: "Sit", weight: 7},
+         {text: "Amet", weight: 5}
+         // ...as many words as you want
+     ];
+
       $scope.getAnswers=function(){
         $http.get('/api/antwoordData/' + $scope.lesID +'/' + $scope.vraag._id)
             .success(function(data) {
